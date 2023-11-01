f551da65a90377fe087485f007a51ca0a5e6c237
mhaneefleghari9 <m.haneef1966@gmail.com>
2023-11-01 14:21:58 +0500

    all changes set

--- api/urls.py
+++ api/urls.py
@@ -6,8 +6,16 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('',views.index, name='index')
-    
+    path('',views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('appointment/', views.appointment, name='appoint'),
+    path('service/', views.service, name='service'),
+    path('contact/', views.contact, name='contact'),
+    path('feature/', views.feature, name='feature'),
+    path('team/', views.team, name='team'),
+    path('testi/', views.testimonial, name='testi'),
+    path('four_0_four/', views.four_0_four, name='four'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
--- api/views.py
+++ api/views.py
@@ -2,26 +2,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def index(request):
     return render(request, 'techworld/index.html')
+
 
 def four_0_four(request):
     return render(request, 'techworld/404.html')
 
+
 def about(request):
     return render(request, 'techworld/about.html')
+
 
 def appointment(request):
     return render(request, 'techworld/appointment.html')
 
+
 def contact(request):
     return render(request, 'techworld/contact.html')
+
 
 def feature(request):
     return render(request, 'techworld/feature.html')
 
+
 def service(request):
     return render(request, 'techworld/service.html')
 
+
 def team(request):
     return render(request, 'techworld/team.html')
+
+def testimonial(request):
+    return render(request, 'techworld/testimonial.html')
